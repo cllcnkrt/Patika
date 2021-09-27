afc4a7a99edb752a5fc26cd98fb7990d98916d31
cllcnkrt <cllcnkrt@outlook.com>
2021-09-27 14:18:39 +0300

    react homework 1

---
+++ react_hw1/src/lib/service.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+
+async function getData(number){
+    const data = await axios(`https://jsonplaceholder.typicode.com/users/${number}`)
+    console.log(data)
+}
+
+export default getData
